9e828eaabda50301411d93e89ef16da7828e3970
Luca <Luca.Rettenberger@kit.edu>
2024-05-22 16:15:09 +0200

    init

---
+++ dataset/ocr.sh
@@ -0,0 +1,6 @@
+nougat 1/3_SI.pdf -o 1/ -m 0.1.0-base --no-skipping --recompute
+nougat 2/4.pdf -o 2/ -m 0.1.0-base --no-skipping --recompute
+nougat 2/4_SI.pdf -o 2/ -m 0.1.0-base --no-skipping --recompute
+nougat 3/5.pdf -o 3/ -m 0.1.0-base --no-skipping --recompute
+nougat 5/7.pdf -o 5/ -m 0.1.0-base --no-skipping --recompute
+nougat 5/7_SI.pdf -o 5/ -m 0.1.0-base --no-skipping --recompute
